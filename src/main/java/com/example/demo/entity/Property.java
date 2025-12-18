0aa53089001006c47a767f84e0c89fbe0ecda051
bala-amypo <bala@amypo.in>
2025-12-18 08:34:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -1,16 +1,12 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import java.util.List;
-
 @Entity
 public class Property {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
 
     private String address;
@@ -39,39 +35,30 @@ public class Property {
     public String getAddress() {
         return address;
     }
-
     public String getCity() {
         return city;
     }
-
     public Double getPrice() {
         return price;
     }
-
     public Double getAreaSqFt() {
         return areaSqFt;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
     public void setPrice(Double price) {
         this.price = price;
     }
-
     public void setAreaSqFt(Double areaSqFt) {
         this.areaSqFt = areaSqFt;
     }
