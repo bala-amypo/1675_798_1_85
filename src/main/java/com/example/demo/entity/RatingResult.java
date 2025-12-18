5c97c95b92dc7f2b4c04fd5abead854da0fad550
bala-amypo <bala@amypo.in>
2025-12-18 07:54:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/RatingResult.java
+++ src/main/java/com/example/demo/entity/RatingResult.java
@@ -1,11 +1,8 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-
 @Entity
 public class RatingResult {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
