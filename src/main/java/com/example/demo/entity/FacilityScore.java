1fd344b76a6d09676a8ce25e9728314c7e5d4bae
bala-amypo <bala@amypo.in>
2025-12-18 09:14:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/FacilityScore.java
+++ src/main/java/com/example/demo/entity/FacilityScore.java
@@ -32,47 +32,35 @@ public class FacilityScore {
     private Integer safetyScore;
 
     public FacilityScore() {}
-
     public Long getId() {
         return id;
     }
-
     public Property getProperty() {
         return property;
-    }
-
     public Integer getSchoolProximity() {
         return schoolProximity;
     }
-
     public Integer getHospitalProximity() {
         return hospitalProximity;
     }
-
     public Integer getTransportAccess() {
         return transportAccess;
     }
-
     public Integer getSafetyScore() {
         return safetyScore;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public void setProperty(Property property) {
         this.property = property;
     }
-
     public void setSchoolProximity(Integer schoolProximity) {
         this.schoolProximity = schoolProximity;
     }
-
     public void setHospitalProximity(Integer hospitalProximity) {
         this.hospitalProximity = hospitalProximity;
     }
-
     public void setTransportAccess(Integer transportAccess) {
         this.transportAccess = transportAccess;
     }
