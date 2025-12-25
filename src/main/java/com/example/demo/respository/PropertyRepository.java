9ede904863803c3a4be546b95b7332f36c39fc4b
bala-amypo <bala@amypo.in>
2025-12-25 10:43:11 +0000

    User commit

--- src/main/java/com/example/demo/respository/PropertyRepository.java
+++ src/main/java/com/example/demo/respository/PropertyRepository.java
@@ -3,13 +3,16 @@ package com.example.demo.repository;
 import com.example.demo.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
+    // Derived query
     List<Property> findByCity(String city);
 
-    @Query("SELECT p FROM Property p WHERE p.city = :city")
-    List<Property> findByCityHql(String city);
+    // HQL query (EXACT name required)
+    @Query("select p from Property p where p.city = :city")
+    List<Property> findByCityHql(@Param("city") String city);
 }
