9d0e9224c1f74c33c84b1ac8f0bcb4fc4a235b96
bala-amypo <bala@amypo.in>
2025-12-26 05:11:08 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiError.java
+++ src/main/java/com/example/demo/exception/ApiError.java
@@ -3,26 +3,27 @@ package com.example.demo.exception;
 import java.time.LocalDateTime;
 
 public class ApiError {
-
-    private int status;
     private String message;
+    private int status;
     private LocalDateTime timestamp;
+    private String path;
 
-    public ApiError(int status, String message) {
-        this.status = status;
+    public ApiError(String message, int status, String path) {
         this.message = message;
+        this.status = status;
+        this.path = path;
         this.timestamp = LocalDateTime.now();
     }
 
-    public int getStatus() {
-        return status;
-    }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 
-    public String getMessage() {
-        return message;
-    }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
+    public String getPath() { return path; }
+    public void setPath(String path) { this.path = path; }
 }
