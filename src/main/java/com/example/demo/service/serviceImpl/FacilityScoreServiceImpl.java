0a2f4aae9a9e6ce8cd8a27493560e9fe737b7b6f
bala-amypo <bala@amypo.in>
2025-12-26 05:25:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/serviceImpl/FacilityScoreServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.FacilityScore;
+import com.example.demo.entity.Property;
+import com.example.demo.repository.FacilityScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FacilityScoreServiceImpl {
+    
+    @Autowired
+    private FacilityScoreRepository facilityScoreRepository;
+    
+    public FacilityScore saveFacilityScore(FacilityScore facilityScore) {
+        return facilityScoreRepository.save(facilityScore);
+    }
+    
+    public Optional<FacilityScore> findByProperty(Property property) {
+        return facilityScoreRepository.findByProperty(property);
+    }
+    
+    public Optional<FacilityScore> findById(Long id) {
+        return facilityScoreRepository.findById(id);
+    }
+    
+    public void deleteFacilityScore(Long id) {
+        facilityScoreRepository.deleteById(id);
+    }
+}
