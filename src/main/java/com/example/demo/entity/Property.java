0391fe1077c974ddb4c9e79aee0860d503fab70a
bala-amypo <bala@amypo.in>
2025-12-26 12:42:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -4,139 +4,53 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "properties")
+@Getter
+@Setter
 public class Property {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @NotBlank
     private String title;
-
+    
     @NotBlank
     private String address;
-
+    
     @NotBlank
     private String city;
-
+    
     @NotNull
-    @Min(1)
+    @Min(0)
     private Double price;
-
+    
     @NotNull
-    @Min(100)
+    @Min(0)
     private Double areaSqFt;
-
-    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
+    
+    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
     private FacilityScore facilityScore;
-
-    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
+    
+    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
     private RatingResult ratingResult;
-
+    
     @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RatingLog> ratingLogs = new ArrayList<>();
-
+    
     @ManyToMany(mappedBy = "assignedProperties")
-    private Set<User> assignedUsers = new HashSet<>();
-
-    public Property() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Double getAreaSqFt() {
-        return areaSqFt;
-    }
-
-    public void setAreaSqFt(Double areaSqFt) {
-        this.areaSqFt = areaSqFt;
-    }
-
-    public FacilityScore getFacilityScore() {
-        return facilityScore;
-    }
-
-    public void setFacilityScore(FacilityScore facilityScore) {
-        this.facilityScore = facilityScore;
-    }
-
-    public RatingResult getRatingResult() {
-        return ratingResult;
-    }
-
-    public void setRatingResult(RatingResult ratingResult) {
-        this.ratingResult = ratingResult;
-    }
-
-    public List<RatingLog> getRatingLogs() {
-        return ratingLogs;
-    }
-
-    public void setRatingLogs(List<RatingLog> ratingLogs) {
-        this.ratingLogs = ratingLogs;
-    }
-
-    public Set<User> getAssignedUsers() {
-        return assignedUsers;
-    }
-
-    public void setAssignedUsers(Set<User> assignedUsers) {
-        this.assignedUsers = assignedUsers;
-    }
-
-    // helper for cascade log tests
+    private List<User> assignedUsers = new ArrayList<>();
+    
     public void addRatingLog(RatingLog log) {
         ratingLogs.add(log);
         log.setProperty(this);
-    }
-
-    public void removeRatingLog(RatingLog log) {
-        ratingLogs.remove(log);
-        log.setProperty(null);
     }
 }
