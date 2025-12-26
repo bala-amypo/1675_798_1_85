24fa44ee24d702324949310e943b3844a685da90
bala-amypo <bala@amypo.in>
2025-12-26 08:03:25 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,36 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+public interface UserService {
 
-@Service
-public class UserService {
-    
-    @Autowired
-    private UserRepository userRepository;
-    
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-    
-    public Optional<User> findByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-    
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-    
-    public Optional<User> findById(Long id) {
-        return userRepository.findById(id);
-    }
-    
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
-    }
+    User register(User user);
+
+    User findByEmail(String email);
 }
