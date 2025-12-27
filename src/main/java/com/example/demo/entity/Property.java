0e444c908a90851846a65887be56941a57eef08c
bala-amypo <bala@amypo.in>
2025-12-27 09:34:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 public class Property {
@@ -23,26 +24,47 @@ public class Property {
 
     private double areaSqFt;
 
+    @OneToMany(mappedBy = "property")
+    private List<RatingLog> ratingLogs;
+
+    @OneToMany(mappedBy = "property")
+    private List<User> assignedUsers;
+
     @NotBlank
     private String name;
 
     @NotBlank
     private String location;
 
-    // Existing getters/setters
+    // ALL getters and setters required by test
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public double getAreaSqFt() { return areaSqFt; }
+    public void setAreaSqFt(double areaSqFt) { this.areaSqFt = areaSqFt; }
+
+    public List<RatingLog> getRatingLogs() { return ratingLogs; }
+    public void setRatingLogs(List<RatingLog> ratingLogs) { this.ratingLogs = ratingLogs; }
+    public void addRatingLog(RatingLog ratingLog) { this.ratingLogs.add(ratingLog); }
+
+    public List<User> getAssignedUsers() { return assignedUsers; }
+    public void setAssignedUsers(List<User> assignedUsers) { this.assignedUsers = assignedUsers; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
     public String getLocation() { return location; }
     public void setLocation(String location) { this.location = location; }
-
-    // NEW setters required by test
-    public void setTitle(String title) { this.title = title; }
-    public void setAddress(String address) { this.address = address; }
-    public void setCity(String city) { this.city = city; }
-    public void setPrice(double price) { this.price = price; }
-    public void setAreaSqFt(double areaSqFt) { this.areaSqFt = areaSqFt; }
 }
