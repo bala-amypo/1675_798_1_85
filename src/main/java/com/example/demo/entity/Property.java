7e14fea9e5836c04709284a4ef5ece32a821b3f1
bala-amypo <bala@amypo.in>
2025-12-27 09:28:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -11,11 +11,25 @@ public class Property {
     private Long id;
 
     @NotBlank
+    private String title;
+
+    @NotBlank
+    private String address;
+
+    @NotBlank
+    private String city;
+
+    private double price;
+
+    private double areaSqFt;
+
+    @NotBlank
     private String name;
 
     @NotBlank
     private String location;
 
+    // Existing getters/setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -24,4 +38,11 @@ public class Property {
 
     public String getLocation() { return location; }
     public void setLocation(String location) { this.location = location; }
+
+    // NEW setters required by test
+    public void setTitle(String title) { this.title = title; }
+    public void setAddress(String address) { this.address = address; }
+    public void setCity(String city) { this.city = city; }
+    public void setPrice(double price) { this.price = price; }
+    public void setAreaSqFt(double areaSqFt) { this.areaSqFt = areaSqFt; }
 }
