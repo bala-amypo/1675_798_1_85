b1e93ab2adde329fb3a851d18206f7d7fd20ac56
bala-amypo <bala@amypo.in>
2025-12-27 10:22:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/respository/PropertyRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+    List<Property> findByCity(String city);
+    List<Property> findByCityHql(String city);  // HQL method name
+}
