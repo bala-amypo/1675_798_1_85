e1b74357b6f603eb29fbca53505024c28ccb3585
bala-amypo <bala@amypo.in>
2025-12-28 04:42:41 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,24 +1,9 @@
 package com.example.demo.dto;
 
 public class AuthResponse {
-
     private String token;
-
-    // Default constructor for Jackson
-    public AuthResponse() {
-    }
-
-    // Constructor for easy creation
     public AuthResponse(String token) {
         this.token = token;
     }
-
-    // Getter & Setter
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
+    public String getToken() { return token; }
 }
--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,38 +1,6 @@
 package com.example.demo.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
 public class LoginRequest {
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
     private String email;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
-
-    // Default constructor
-    public LoginRequest() {
-    }
-
-    // Getters
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    // Setters
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
