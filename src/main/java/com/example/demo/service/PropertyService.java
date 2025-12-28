2b7471a4aa3b279080c44f564720880c24ec391f
bala-amypo <bala@amypo.in>
2025-12-28 06:11:32 +0000

    User commit

--- src/main/java/com/example/demo/service/PropertyService.java
+++ src/main/java/com/example/demo/service/PropertyService.java
@@ -11,3 +11,19 @@
 //     Property updateProperty(Long id, Property property);
 //     void deleteProperty(Long id);
 // }
+package com.example.demo.service;
+
+import com.example.demo.entity.Property;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface PropertyService {
+
+    // REQUIRED BY TEST
+    Property addProperty(Property property);
+
+    // used by controller
+    Page<Property> listProperties(Pageable pageable, String city);
+
+    Property getProperty(Long id);
+}
--- src/main/java/com/example/demo/service/RatingService.java
+++ src/main/java/com/example/demo/service/RatingService.java
@@ -6,3 +6,11 @@
 //     RatingResult generateRating(Long propertyId);
 //     RatingResult getRatingByPropertyId(Long propertyId);
 // }
+package com.example.demo.service;
+
+import com.example.demo.entity.RatingResult;
+
+public interface RatingService {
+    RatingResult generateRating(Long propertyId);
+    RatingResult getRating(Long propertyId);
+}
